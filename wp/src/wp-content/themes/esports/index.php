8eff1bcbc7ce73f760311b33b5b92242a9e0b926
c0de4un <code4un@yandex.ru>
2022-07-09 16:49:37 +0400

    [003] Add empty theme

---
+++ wp/src/wp-content/themes/esports/index.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * ESports theme
+ *
+ * @package WordPress
+ * @subpackage ESports
+ * @since ESports ESports 0.1
+ */
+
+get_header(); ?>
+
+<main id="site-content">
+</main>
+
+<?php get_footer();
